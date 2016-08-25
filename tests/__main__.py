af142ef6f958f081f65487a83b0b56fc0636b1d0
Valerii Lashmanov <vflashm@gmail.com>
2016-08-25 15:01:47 -0400

    tests overhaul

--- tests/__main__.py
+++ tests/__main__.py
@@ -5,141 +5,186 @@ import model
 import interpreter
 import error
 import compiler
+import parse
+import transpiler
+import traceback
 
 logger = logging.getLogger('test')
 
+ERROR_TYPES = (
+    error.CompileTimeError,
+    error.CodeSyntaxError,
+    parse.ParserError,
+    model.ModelError,
+    transpiler.InlinerError,
+    error.ExecutionTimeError,
+    error.InterpreterError,
+    )
+ERRORS = dict((e.__name__, e) for e in ERROR_TYPES)
+ERRORS['RuntimeError'] = error.ExecutionTimeError # useful alias
+ERRORS['SyntaxError'] = error.CodeSyntaxError # useful alias
+
 class TestFailure(Exception):
-    pass
+    def __init__(self, msg, code):
+        Exception.__init__(self, msg)
+        self.code = code
+        self.cause = None
 
 class NoSuccess(TestFailure):
-    pass
+    def __init__(self, code, cause):
+        TestFailure.__init__(self, 'received %s' % type(cause).__name__, code)
+        self.cause = cause
 
 class NoFailure(TestFailure):
-    pass
+    def __init__(self, code, expected):
+        TestFailure.__init__(self, 'expected %s(%s)' % (expected[0].__name__, expected[1]), code)
+        self.expected = expected
 
 class WrongFailure(TestFailure):
-    pass
-
-def runtime_check(path):
-    prefix = os.path.basename(path)
-    lines = open(path).readlines()
-    for idx, line in enumerate(lines):
-        if '//<error' in line and not line.strip().startswith('//'):
-            lines[idx] = '\n'
-
-    content = ''.join(lines)
-    m = interpreter.build_model(content)
+    def __init__(self, code, expected, cause):
+        TestFailure.__init__(self, 'expected %s(%s), received %s' % (expected[0].__name__, expected[1], type(cause).__name__), code)
+        self.expected = expected
+        self.cause = cause
     
-    try:
-        m.resolve_term('main', None)
-    except model.Undefined:
-        return
-    
-    good_lines = []
-    bad_line_indices = []
+class TestFile(object):
+    def __init__(self, path, verbose=False):
+        if verbose: print 'Parsing %s' % path
+        self.path = path
+        self.lines = open(path).read().splitlines()
+        self.count = 1
+        self.errors = {}
+        self.output = []
+        self.no_run = False
+        for idx, line in enumerate(self.lines):
+            if '//<' in line:
+                code, command = line.split('//<', 1)
+                if not code.strip().startswith('//'):
+                    parts = command.split(' ', 1)
+                    name = parts[0].strip()
+                    if len(parts) > 1:
+                        value = parts[1].strip()
+                    else:
+                        value = ''
+                    if name == 'Output':
+                        self.output.append(value)
+                    elif name in ERRORS:
+                        self.errors[idx] = ERRORS[name], value
+                        self.count += 1
+                    else:
+                        assert False, 'Unknown test command: %s' % name
+            if line.strip().startswith('//!no_run'):
+                self.no_run = True
 
-    for idx, line in enumerate(lines):
-        if '//<runtime' in line and not line.strip().startswith('//'):
-            bad_line_indices.append(idx)
-        else:
-            good_lines.append(line)
+    def build_code(self, error_idx=None):
+        test_lines = list(self.lines)
+        for idx, line in enumerate(test_lines):
+            if idx in self.errors and idx != error_idx:
+                test_lines[idx] = ''
+        test_code = '\n'.join(test_lines)
+        return test_code
 
-    good = ''.join(good_lines)
-    try:
-        m = interpreter.build_model(good)
-    except error.SyntaxError as e:
-        raise NoSuccess('Syntax error: %s' % e)
-    try:
-        interpreter.run_model(m)
-    except error.InterpreterError as e:
-        raise NoSuccess('Interpreter runtime error: %s' % e)
-    try:
-        compiler.run_model(m, prefix)
-    except compiler.CompilationError as e:
-        raise TestFailure('Unable to compile: %s' % e)
-    except compiler.ExecutionError as e:
-        raise NoSuccess('Compiler runtime error: %s' % e)
-
-    for bad_idx in bad_line_indices:
-        bad_lines = list(lines)
-        test_line = bad_lines[bad_idx]
-        error_message = test_line.split('//<runtime', 1)[1].strip()
-        for other_bad_idx in bad_line_indices:
-            if other_bad_idx != bad_idx:
-                bad_lines[other_bad_idx] = '\n'
-        bad = ''.join(bad_lines)
+    def _check(self, verbose=False):
+        print 'Checking %s' % self.path
+        good = self.build_code(None)
+        if verbose: print 'Checking normal run'
         try:
-            m = interpreter.build_model(bad)
-        except error.SyntaxError as e:
-            raise TestFailure('Unexpected syntax error: %s' % e)
+            if verbose: print 'Building model'
+            m = interpreter.build_model(good)
+            if not self.no_run:
+                if verbose: print 'Checking interpreter'
+                interpreter.run_model(m)
+                if verbose: print 'Checking compiler'
+                compiler.run_model(m)
+        except Exception as e:
+            raise NoSuccess(good, e), None, sys.exc_info()[2]
+
+        for idx, edef in self.errors.items():
+            etype, message = edef
+            bad = self.build_code(idx)
+            if verbose: print 'Checking error run: %s %s' % (etype.__name__, message)
+            if verbose: print 'Building model'
+            try:
+                m = interpreter.build_model(bad)
+            except Exception as e:
+                if not issubclass(type(e), etype) or message not in str(e):
+                    raise WrongFailure(bad, edef, e), None, sys.exc_info()[2]
+                continue
+            else:
+                if issubclass(etype, error.CodeSyntaxError):
+                    raise NoFailure(bad, edef)
+
+            if self.no_run:
+                raise NoFailure(bad, edef)
+
+            if verbose: print 'Checking interpreter'
+            try:
+                interpreter.run_model(m)
+            except Exception as e:
+                if not issubclass(type(e), etype) or message not in str(e):
+                    raise WrongFailure(bad, edef, e), None, sys.exc_info()[2]
+            else:
+                raise NoFailure(bad, edef)
+
+            if verbose: print 'Checking compiler'
+            try:
+                compiler.run_model(m)
+            except error.ExecutionTimeError as e:
+                if not issubclass(type(e), etype) or message not in str(e):
+                    raise WrongFailure(bad, edef, e), None, sys.exc_info()[2]
+            else:
+                raise NoFailure(bad, edef)
+
+    def check(self, verbose=False):
         try:
-            interpreter.run_model(m)
-            raise TestFailure('No interpreter runtime error on line: %s' % test_line)
-        except error.InterpreterError as e:
-            if not error_message.lower() in str(e).lower():
-                raise WrongFailure('Wrong interpreter runtime error on line: %s: %s' % (test_line, e))
-        try:
-            compiler.run_model(m, prefix)
-            raise TestFailure('No compiler runtime error on line: %s' % test_line)
-        except compiler.CompilationError as e:
-            raise TestFailure('Unable to compile: %s' % e)
-        except compiler.ExecutionError as e:
-            if not error_message.lower() in str(e).lower():
-                raise WrongFailure('Wrong compiler runtime error on line: %s: %s' % (test_line, e))
-
-
-def check(path):
-    lines = open(path).readlines()
-
-    # syntax checks
-    good_lines = []
-    bad_line_indices = []
-        
-    for idx, line in enumerate(lines):
-        if '//<error' in line and not line.strip().startswith('//'):
-            bad_line_indices.append(idx)
-        else:
-            good_lines.append(line)
-
-    good = ''.join(good_lines)
-    try:
-        m = interpreter.build_model(good)
-    except error.SyntaxError as e:
-        raise NoSuccess('Syntax error: %s' % e)
-    
-    for bad_idx in bad_line_indices:
-        bad_lines = list(lines)
-        test_line = bad_lines[bad_idx]
-        error_message = test_line.split('//<error', 1)[1].strip()
-        for other_bad_idx in bad_line_indices:
-            if other_bad_idx != bad_idx:
-                bad_lines[other_bad_idx] = '\n'
-        bad = ''.join(bad_lines)
-        try:
-            interpreter.build_model(bad)
-            raise TestFailure('No error on line: %s' % test_line)
-        except error.SyntaxError as e:
-            if not error_message.lower() in str(e).lower():
-                raise WrongFailure('Wrong error on line: %s: %s' % (test_line, e))
-
-    # runtime checks
-    runtime_check(path)
-                
-def check_all(root):
-    success = True
-    for name in os.listdir(root):
-        if not name.endswith('.epl'):
-            continue
-        path = os.path.join(root, name)
-        logger.info('Checking: %s', path)
-        try:
-            check(path)
-            logger.info('SUCCESS: %s', path)
+            self._check(verbose)
+            return True
         except TestFailure as e:
-            logger.warn('FAILURE: %s %s %s', path, type(e), e)
-            success = False
-    return success
+            if verbose:
+                traceback.print_exc()
+                if e.cause:
+                    print 'Cause: %s(%s)' % (type(e.cause).__name__, e.cause)
+                print 'For code:'
+                print e.code
+            else:
+                print e
 
-logging.basicConfig(level=logging.DEBUG)
-if not check_all(os.path.dirname(__file__)):
-    sys.exit(1)
+def gather_tests(path, verbose=False):
+    if os.path.isfile(path):
+        return [TestFile(path, verbose)]
+    else:
+        res = []
+        for name in os.listdir(path):
+            ipath = os.path.join(path, name)
+            if not name.startswith('.') and (os.path.isdir(ipath) or name.endswith('.epl')):
+                res += gather_tests(ipath, verbose)
+        return res
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', nargs='*')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+    
+    test_set = []
+    if args.path:
+        for path in args.path:
+            test_set += gather_tests(path, args.verbose)
+    else:
+        root = os.path.dirname(__file__)
+        test_set += gather_tests(root, args.verbose)
+            
+    test_count = sum(t.count for t in test_set)
+    print 'Collected %s test files with %s test cases' % (len(test_set), test_count)
+    
+    success = True
+    for test_file in test_set:
+        if not test_file.check(args.verbose):
+            success = False
+    if success:
+        print 'All succeeded'
+    else:
+        print 'Some tests failed'
+        sys.exit(1)
